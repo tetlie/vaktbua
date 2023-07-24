f2d5fa1f9bc7944e1c358271265e38483f67950e
Marius Tetlie <marius.tetlie@gmail.com>
2023-07-24 19:17:10 +0200

    wip: tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,19 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    fontFamily: {
+      sans: ["Helvetica", "Arial", "sans-serif"],
+      serif: ["Times New Roman", "Times", "serif"],
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        black: "#1E1E1E",
+        white: "white",
       },
     },
+  },
+  future: {
+    hoverOnlyWhenSupported: true,
   },
   plugins: [],
 }
