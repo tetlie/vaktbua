3926fd851f2e88c39d5442b202c754b4751331e2
Marius Tetlie <marius.tetlie@gmail.com>
2023-07-29 22:47:50 +0200

    feat: header with working opening hours and menu items

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,19 +1,58 @@
 import React from "react";
-import Link from "next/link";
 import ClientSideRoute from "./ClientSideRoute";
+import { PATH_EVENTS } from "@/lib/constants/paths";
 
-const Header = () => {
+type Props = {
+  globals: Globals;
+};
+
+const Header = ({ globals }: Props) => {
+  const { openingHours } = globals;
+
+  const openingHoursGreeting = () => {
+    const today = new Date();
+    const day = today.getDay();
+
+    const days = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
+    const norwegianDays = ["søndag", "mandag", "tirsdag", "onsdag", "torsdag", "fredag", "lørdag"];
+
+    const dayTitle = norwegianDays[day];
+
+    const dayKey = days[day];
+    const hours = openingHours[dayKey as keyof typeof openingHours];
+
+    const isClosed =
+      hours === "" || hours === "Stengt" || hours === "stengt" || hours === undefined;
+
+    const stringStart = `I dag (${dayTitle}) er Vaktbua`;
+    const stringEnd = isClosed ? "stengt" : ` åpent ${hours}`;
+    const string = `${stringStart} ${stringEnd}`;
+
+    return string;
+  };
+
   return (
     <header>
-      <div className="flex flex-col items-center justify-center w-full">
-        <p className="font-serif text-3xl font-normal leading-[0.96em] tracking-tighter text-center md:text-4xl lg:text-5xl">
-          I dag (lørdag) er Vaktbua åpent 14:00 – 02:30
-        </p>
-        <ClientSideRoute route={`/`}>
-          <h1 className="mt-5 font-serif font-bold tracking-tighter text-black lg:mt-10 text-7xl md:text-8xl lg:text-9xl">
-            Vaktbua
-          </h1>
-        </ClientSideRoute>
+      <div className="flex items-start justify-between w-full">
+        <div className="w-1/4">
+          <ClientSideRoute route={`/`}>
+            <p className="hover:underline text-lg text-left font-normal leading-[0.96em] tracking-tight  lg:text-xl">
+              Hjem
+            </p>
+          </ClientSideRoute>
+        </div>
+        <div className="grow">
+          <p className="font-serif text-3xl font-normal leading-[0.96em] tracking-tighter text-center md:text-4xl lg:text-5xl">
+            {openingHoursGreeting()}
+          </p>
+        </div>
+        <div className="w-1/4">
+          <ClientSideRoute route={`/${PATH_EVENTS}`}>
+            <p className="hover:underline text-lg font-normal text-right leading-[0.96em] tracking-tight lg:text-xl">
+              Events
+            </p>
+          </ClientSideRoute>
+        </div>
       </div>
     </header>
   );
