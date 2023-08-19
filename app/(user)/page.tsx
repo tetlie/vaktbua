eec9f7fa62ee96fc74ba9f835317cd7ca2664055
Marius Tetlie <marius.tetlie@gmail.com>
2023-08-19 20:37:40 +0200

    wip: eventlist logic

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,15 +1,18 @@
-import Image from "next/image";
 import EventList from "../components/EventList";
 import { groq } from "next-sanity";
 import { client } from "@/sanity/lib/client";
 import EventFeatured from "../components/EventFeatured";
 import OpeningHours from "../components/OpeningHours";
 import PageTitle from "../components/PageTitle";
+import moment from "moment";
+
+import "moment-timezone";
 
 const query = groq`{
   "globals": *[_type == "globals"][0] {
   ...,
   },
+  "nextEvent": *[_type == "event" && dateTimeStart > now()] | order(dateTimeStart asc) [0],
   "events": *[_type == "event"] {
   ...,
   categories[]->
@@ -20,7 +23,16 @@ export const revalidate = 60;
 
 export default async function Home() {
   const data = await client.fetch(query);
-  const { events, globals } = data;
+  const { nextEvent, events, globals } = data;
+
+  const curWeekStart = moment().tz("Europe/Berlin").startOf("week").toDate().toISOString();
+  const curWeekEnd = moment().tz("Europe/Berlin").endOf("week").toDate().toISOString();
+
+  const eventsThisWeek = events.filter((event: Event) => {
+    const eventDate = new Date(event.dateTimeStart).toISOString();
+    return eventDate >= curWeekStart && eventDate <= curWeekEnd;
+  });
+
   return (
     <div>
       <PageTitle title={globals.title} />
@@ -33,14 +45,14 @@ export default async function Home() {
       </section>
       <section className="items-center w-full h-full mt-5 space-y-5 lg:mt-10 ">
         <div className="w-full">
-          <EventFeatured event={events[0]} />
+          <EventFeatured event={nextEvent} />
         </div>
         <div className="w-full">
-          <EventList events={events} />
+          <EventList heading="Denne uken på Vaktbua" events={eventsThisWeek} />
         </div>
       </section>
       <section className="flex items-center justify-center w-full mt-5 space-x-10 lg:mt-10">
-        <div className="leading-[0.96em] text-6xl tracking-tight text-center  md:text-7xl lg:text-8xl">
+        <div className="leading-[0.96em] text-6xl tracking-tight text-center md:text-7xl lg:text-8xl">
           <p>Dele</p>
           <p>Samarbeide</p>
           <p>Delta</p>
@@ -48,7 +60,7 @@ export default async function Home() {
       </section>
       <section className="mt-5 lg:mt-10">
         <div className="w-full">
-          <OpeningHours />
+          <OpeningHours openingHours={globals.openingHours} />
         </div>
       </section>
     </div>
--- app/components/EventList.tsx
+++ app/components/EventList.tsx
@@ -4,32 +4,38 @@ import dateFormatter from "@/lib/dateFormatter";
 import { PATH_EVENTS } from "@/lib/constants/paths";
 
 type Props = {
+  heading?: string;
   events: Event[];
 };
 
-const EventList = ({ events }: Props) => {
+const EventList = ({ heading = "Arrangementer", events }: Props) => {
   return (
     <div className="h-full p-5 bg-black lg:p-10 text-gray">
       <h2 className="font-serif text-3xl font-bold tracking-tighter text-center md:text-4xl lg:text-5xl">
-        Arrangementer
+        {heading}
       </h2>
       <ul className="mt-5 lg:mt-10">
         {events &&
-          events.map((event) => (
-            <li
-              key={event._id}
-              className="py-1 transition-all ease-in-out border-b hover:bg-gray hover:text-black hover:px-10 border-b-gray group"
-            >
-              <ClientSideRoute route={`/${PATH_EVENTS}/${event.slug.current}`}>
-                <div className="flex justify-between text-lg lg:text-xl">
-                  <h3 className="leading-snug truncate">{event.title}</h3>
-                  <p className="w-1/3 leading-snug text-right">
-                    {dateFormatter(event.dateTimeStart)}
-                  </p>
-                </div>
-              </ClientSideRoute>
-            </li>
-          ))}
+          events.map((event) => {
+            const isPastEvent = new Date(event.dateTimeStart) < new Date();
+            return (
+              <li
+                key={event._id}
+                className={`${
+                  isPastEvent && "line-through"
+                } py-1 transition-all ease-in-out border-b hover:bg-gray hover:text-black hover:px-10 border-b-gray group`}
+              >
+                <ClientSideRoute route={`/${PATH_EVENTS}/${event.slug.current}`}>
+                  <div className="flex justify-between text-lg lg:text-xl">
+                    <h3 className="leading-snug truncate">{event.title}</h3>
+                    <p className="w-1/3 leading-snug text-right">
+                      {dateFormatter(event.dateTimeStart)}
+                    </p>
+                  </div>
+                </ClientSideRoute>
+              </li>
+            );
+          })}
       </ul>
       <ClientSideRoute route={`/${PATH_EVENTS}`}>
         <p className="mt-5 text-lg text-center hover:underline lg:mt-10 lg:text-xl">
